18350a017ee1110d6db9db88056981d71f28a1d1
Alan Weingartner <alan.weingartner@gmail.com>
2016-11-30 16:51:08 -0200

    20161130 16:50

--- js/script.js
+++ js/script.js
@@ -5,9 +5,65 @@ $(document).ready(function ()
         naturalHeight: 600,
         imageSrc: 'img/ps-marquee1440x660.jpg'
     });
-    
-    $('#overview').on('click', function ()
+
+    $('#overview, #whats-new, #learn-and-support, #free-trial, #buy-now').on('click', function ()
     {
-        $('#header-content').html('nada');
+        new Spinner({
+            lines: 13,
+            length: 28,
+            width: 14,
+            radius: 42,
+            scale: 1,
+            corners: 1,
+            opacity: 0.25,
+            speed: 1,
+            trail: 60,
+            color: '#1B3D47',
+            direction: 1,
+            fps: 20,
+            position: 'absolute',
+            top: '0',
+            left: '50%'
+        }).spin(document.getElementById('header-content'));
+
+        getPage('pages/' + this.id + '.html').then(
+            function (success)
+            {
+                $('#header-content').html(success);
+            },
+            function (error)
+            {
+                var message = 'an unknown error has occurred :(';
+                if (error.status == 404)
+                    message = 'sorry, the requested page was not found'
+                swal({
+                    title: 'ERROR',
+                    text: message,
+                    type: 'warning',
+                    allowOutsideClick: true,
+                    confirmButtonText: 'OK'
+                });
+            }
+        );
     });
 });
+
+function getPage(url) {
+
+    var promise = $.Deferred();
+
+    $.ajax({
+        url: url,
+        async: true,
+        method: 'GET',
+        success: function (response) {
+            promise.resolve(response);
+        },
+        error: function (error) {
+            promise.reject(error);
+        }
+    });
+
+    return promise;
+
+}
